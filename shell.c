290a43a274975edd7d83b602316fc350905ddf1b
aalaaimam <aalaaimam@gmail.com>
2024-04-22 10:03:53 -0700

    simple_shell

--- shell.c
+++ shell.c
@@ -18,9 +18,9 @@
 int main(void)
 {
 /*  Remove unused variable 'command' */
-/* char command[MAX_COMMAND_LENGTH]; */
+// char command[MAX_COMMAND_LENGTH]; */
 
-/* Your code for the shell program */
+// Your code for the shell program
 
 return 0; // Return 0 at the end of main
 }
