2418c0331f57fd5e15a742545f28e445ce1b208f
aalaaimam <aalaaimam@gmail.com>
2024-04-22 09:41:15 -0700

    simple_shell

--- shell.c
+++ shell.c
@@ -1,7 +1,9 @@
+// shell.c
+
 #include <stdio.h>
 #include <stddef.h>
 #include <string.h>
-#include "shell.h"
+#include "shell.h"  // Include any necessary header files
 
 #define MAX_COMMAND_LENGTH 1024
 
@@ -17,24 +19,5 @@ int main(void)
 {
 char command[MAX_COMMAND_LENGTH];
 
-while (1)
-{
-printf("#cisfun$ ");
-
-if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
-{
-perror("fgets");
-return (1); /* Exit if Ctrl+D (EOF) is pressed */
-}
-
-/* Remove newline character if present */
-if ((strlen(command) > 0) && (command[strlen(command) - 1] == '\n'))
-{
-command[strlen(command) - 1] = '\0';
-}
-
-/* Your code to process the command goes here */
-}
-
-return (0);
+// Your code for the shell program
 }
